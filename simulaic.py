484b1fd28cf628d3f818d7f8643ab38ccd4ae668
Matthew Lewis <matt@mplewis.com>
2013-03-04 15:36:52 -0600

    working code; no resize func yet

--- simulaic.py
+++ simulaic.py
@@ -8,11 +8,6 @@ import argparse
 import sys
 import os
 
-parser = argparse.ArgumentParser(description='Convert an image to a new ' + 
-                                             'size and color fidelity.')
-
-parser.parse_args()
-
 # contains the target rasterized color values for the image
 PALETTE = []
 
@@ -20,17 +15,44 @@ PALETTE = []
 COLORS = [
     [0,   0,   0  ], # black
     [255, 255, 255], # white
+
     [255, 0,   0  ], # red
     [0,   255, 0  ], # green
     [0,   0,   255], # blue
+
     [0,   255, 255], # cyan
     [255, 0,   255], # magenta
     [255, 255, 0  ]  # yellow
 ]
 
-# add the first NUMCOLORS colors from COLORS to PALETTE
-NUMCOLORS = 99
+parser = argparse.ArgumentParser(description='Convert an image to a new ' + 
+                                             'size and color fidelity.')
+parser.add_argument('image', nargs='*', 
+                    help='The image or images to be converted.')
+parser.add_argument('-c', '--colors', type=int,
+                    help='Number of colors to use in converting the ' + 
+                    ' image. Maximum (default) is ' + str(len(COLORS)) + '.')
+parser.add_argument('-w', '--width', type=int,
+                    help='Width of the output image, in pixels.')
+parser.add_argument('-t', '--height', type=int,
+                    help='Height of the output image, in pixels.')
 
+args = parser.parse_args()
+
+if args.colors != None:
+    NUMCOLORS = args.colors
+else:
+    NUMCOLORS = len(COLORS)
+
+DO_RESIZE = False
+if args.height != None and args.width != None:
+    DO_RESIZE = True
+    HEIGHT = args.height
+    WIDTH = args.width
+    NEW_SIZE = (WIDTH, HEIGHT)
+    print 'Resizing images to ' + str(WIDTH) + 'x' + str(HEIGHT)
+
+# add the first NUMCOLORS colors from COLORS to PALETTE
 for colornum in range(NUMCOLORS):
     try:
         PALETTE += COLORS.pop(0)
@@ -38,26 +60,30 @@ for colornum in range(NUMCOLORS):
         print('WARNING: ' + str(NUMCOLORS) + ' colors requested but only ' + 
               str(colornum) + ' colors available, using the first ' + 
               str(colornum) + ' colors')
+        NUMCOLORS = str(colornum)
         break
 
 # pad PALETTE to 768 values (256 * RGB (3) = 768)
 if len(PALETTE) < 768:
     PALETTE += [0] * (768 - len(PALETTE))
 
-for imgfn in sys.argv[1:]:
-    dirname, filename = os.path.split(imgfn)
+for imgpath in args.image:
+    dirname, filename = os.path.split(imgpath)
     name, ext = os.path.splitext(filename)
+    newpathname = os.path.join(dirname, "conv-%s.png" % name)
+    print('Processing ' + imgpath + ' to ' + str(NUMCOLORS) + ' colors as ' +
+          newpathname)
+
     # a palette image to use for quant
     pimage = Image.new("P", (1, 1), 0)
     pimage.putpalette(PALETTE)
 
     # open the source image
-    image = Image.open(sys.argv[1])
+    image = Image.open(imgpath)
     image = image.convert("RGB")
 
     # quantize it using our palette image
     imagep = image.quantize(palette=pimage)
 
     # save
-    newpathname = os.path.join(dirname, "cga-%s.png" % name)
     imagep.save(newpathname)
