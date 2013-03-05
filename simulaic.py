78e8990c7f9b456ba7178bbe08663a7610d41f76
Matthew Lewis <matt@mplewis.com>
2013-03-04 19:53:10 -0600

    add doc comments

--- simulaic.py
+++ simulaic.py
@@ -37,15 +37,18 @@ PARSER.add_argument('-w', '--width', type=int,
 PARSER.add_argument('-e', '--height', type=int,
                     help='Height of the output image, in pixels.')
 
+# store parsed args in ARGS
 ARGS = PARSER.parse_args()
 
 IMGPATH = ARGS.image
 
+# use default theme if none selected
 if ARGS.theme != None:
     SELECTED_THEME = ARGS.theme
 else:
     SELECTED_THEME = DEFAULT_THEME
 
+# resize if both height and width are specified
 DO_RESIZE = False
 if ARGS.height != None and ARGS.width != None:
     DO_RESIZE = True
@@ -63,12 +66,16 @@ for TRIPLE in THEMES[SELECTED_THEME]:
 if len(PALETTE) < 768:
     PALETTE += [0] * (768 - len(PALETTE))
 
+# split the input name apart into its useful elements
 dirname, filename = os.path.split(IMGPATH)
 name, ext = os.path.splitext(filename)
+# output naming convention:
+#     input file:  "mypic.jpg", filter: BW
+#     output file: "conv-mypic-BW.jpg"
 newpathname = os.path.join(dirname, 'conv-' + name + '-' + SELECTED_THEME +
                            '.png')
 
-# a palette image to use for quant
+# use the palette we defined earlier
 pimage = Image.new("P", (1, 1), 0)
 pimage.putpalette(PALETTE)
 
@@ -76,7 +83,7 @@ pimage.putpalette(PALETTE)
 imagef = Image.open(IMGPATH)
 imagec = imagef.convert("RGB")
 
-# resize it to our target size
+# resize it to our target size, if requested
 if DO_RESIZE:
     print 'Resizing image to ' + str(WIDTH) + 'x' + str(HEIGHT)
     imagec = imagec.resize(NEW_SIZE, Image.ANTIALIAS)
