0ee89eee950ab9e318be0817f813927f9e86eea6
Matthew Lewis <matt@mplewis.com>
2013-03-04 19:52:52 -0600

    themes moved to themes.py

--- simulaic.py
+++ simulaic.py
@@ -9,68 +9,13 @@ import Image
 import argparse
 import os
 
+# THEMES is the list of color palettes that can be selected for the output image
+# it's stored in themes.py
+from themes import THEMES
+
 # DEFAULT_THEME is picked whenever no other theme is selected at the
 # command line
 DEFAULT_THEME = 'BW'
-
-# THEMES is the list of color palettes that can be selected for the output image
-THEMES = {
-    'BW': [
-        [0,   0,   0  ], # black
-        [255, 255, 255], # white
-    ],
-    'GREY5': [
-        [0,   0,   0  ], # black
-        [64,  64,  64 ], # grey 25%
-        [128, 128, 128], # grey 50%
-        [192, 192, 192], # grey 75%
-        [255, 255, 255], # white
-    ],
-    'BWRGBCMY': [
-        [0,   0,   0  ], # black
-        [255, 255, 255], # white
-
-        [255, 0,   0  ], # red
-        [0,   255, 0  ], # green
-        [0,   0,   255], # blue
-
-        [0,   255, 255], # cyan
-        [255, 0,   255], # magenta
-        [255, 255, 0  ]  # yellow
-    ],
-    'BWRGB': [
-        [0,   0,   0  ], # black
-        [255, 255, 255], # white
-
-        [255, 0,   0  ], # red
-        [0,   255, 0  ], # green
-        [0,   0,   255], # blue
-    ],
-    'PLAYPEN': [
-        [221, 43,  17 ], # red
-        [242, 79,  0  ], # orange
-        [235, 186, 5  ], # yellow
-        [1,   108, 56 ], # green
-        [26,  94,  177]  # blue
-    ],
-    'PLAYPENBW': [
-        [0,   0,   0  ], # black
-        [255, 255, 255], # white
-        [221, 43,  17 ], # red
-        [242, 79,  0  ], # orange
-        [235, 186, 5  ], # yellow
-        [1,   108, 56 ], # green
-        [26,  94,  177]  # blue
-    ],
-    'BWCMY': [
-        [0,   0,   0  ], # black
-        [255, 255, 255], # white
-
-        [0,   255, 255], # cyan
-        [255, 0,   255], # magenta
-        [255, 255, 0  ]  # yellow
-    ]
-}
 
 AVAILABLE_THEMES = []
 for THEME_NAME in THEMES:
@@ -78,6 +23,8 @@ for THEME_NAME in THEMES:
 
 AVAILABLE_THEMES_STR = ', '.join(AVAILABLE_THEMES)
 
+
+# set up the parser
 PARSER = argparse.ArgumentParser(description='Convert an image to a new ' + 
                                              'size and color fidelity.')
 PARSER.add_argument('image', help='The image to be converted.')
